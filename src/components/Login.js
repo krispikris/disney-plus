dcc09a6083a2007f3c98b44d44924e4129fabb3b
krispikris <kris@kristopherhan.com>
2023-12-06 04:36:55 -0800

    splash page complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,7 +5,14 @@ const Login = (props) => {
     <Container>
       <Content>
         <CTA>
-          <CTALogoOne src="/images/01-splash/01-cta-logo-one.png" alt="" />
+          <CTALogoOne src="/images/01-splash/01-cta-logo-one.svg" alt="" />
+          {/* <CTALogoOne src="/images/01-splash/01-cta-logo-one.png" alt="" /> */}
+          <SignUp>GET ALL THERE</SignUp>
+          <Description>
+            Get Premier Access to epic entertainment from Hulu, Disney+, and
+            ESPN+ with a Disney Bundle.
+          </Description>
+          <CTALogoTwo src="/images/01-splash/02-cta-logo-two.png" alt="" />
         </CTA>
         <SplashBackground />
       </Content>
@@ -56,29 +63,77 @@ const SplashBackground = styled.div`
 const CTA = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  flex-wrap: wrap;
-  align-items: center;
-  text-align: center;
 
   width: 100%;
   max-width: 650px;
-  margin-top: 0;
-  margin-right: auto;
-  margin-left: auto;
-
-  transition-timing-function: ease-out;
-  transition: opacity 0.2s;
 
   border: 1px solid blue;
 `;
 
+// const CTA = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   flex-wrap: wrap;
+//   align-items: center;
+//   text-align: center;
+
+//   width: 100%;
+//   max-width: 650px;
+//   margin-top: 0;
+//   margin-right: auto;
+//   margin-left: auto;
+
+//   transition-timing-function: ease-out;
+//   transition: opacity 0.2s;
+
+//   border: 1px solid blue;
+// `;
+
 const CTALogoOne = styled.img`
-  margin-bottom: 12px;
-  max-width: 600px;
-  min-height: 1px;
   display: block;
   width: 100%;
+  max-width: 600px;
+  min-height: 1px;
+  margin-bottom: 12px;
+`;
+
+const SignUp = styled.a`
+  font-size: 18px;
+  font-weight: bold;
+  letter-spacing: 1.5px;
+
+  color: #f9f9f9;
+  background-color: #0063e5;
+
+  width: 100%;
+  padding: 16.5px 0;
+  margin-bottom: 12px;
+
+  border: 1px solid transparent;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: #0483ee;
+  }
+`;
+
+const Description = styled.p`
+  font-size: 12px;
+  color: hsla(0, 0%, 95.3%, 1);
+  letter-spacing: 1.5px;
+  line-height: 1.5;
+
+  margin: 0 0 24px;
+  border: 1px solid pink;
+`;
+
+const CTALogoTwo = styled.img`
+  display: inline-block;
+  vertical-align: bottom;
+  width: 100%;
+  max-width: 600px;
+  margin-bottom: 24px;
 `;
 
 export default Login;

12eff458715f29023db41ff0a400e3c6d61f9a07
krispikris <kris@kristopherhan.com>
2023-12-12 01:52:31 -0800

    sliders added

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,7 +1,103 @@
 import styled from 'styled-components';
+import Slider from 'react-slick';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const ImageSlider = (props) => {
-  return <div>ImageSlider</div>;
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1, // how many slides to show at once
+    slidesToScroll: 1, // how many slides to scroll at once
+    autoplay: true,
+  };
+
+  return (
+    <Carousel {...settings}>
+      <Wrap>
+        <a>
+          <img
+            src="/images/02-home/01-sliders/01-slider-wanda-vision.jpg"
+            alt=""
+          />
+        </a>
+      </Wrap>
+
+      <Wrap>
+        <a>
+          <img src="/images/02-home/01-sliders/02-slider-onward.jpg" alt="" />
+        </a>
+      </Wrap>
+
+      <Wrap>
+        <a>
+          <img src="/images/02-home/01-sliders/03-slider-burrow.jpg" alt="" />
+        </a>
+      </Wrap>
+
+      <Wrap>
+        <a>
+          <img src="/images/02-home/01-sliders/04-slider-bts.jpg" alt="" />
+        </a>
+      </Wrap>
+    </Carousel>
+  );
 };
+
+const Carousel = styled(Slider)`
+  margin-top: 20px;
+
+  & > button {
+    width: 5vw;
+    height: 100%;
+
+    opacity: 0;
+    z-index: 1;
+
+    &:hover {
+      opacity: 1;
+      transition: opacity 0.2s ease 0s;
+    }
+  }
+
+  ul li button {
+    &:before {
+      font-size: 10px;
+
+      color: rgb(150, 158, 171);
+    }
+  }
+
+  // what ever slider is active, make the button before white
+  li.slick-active button:before {
+    color: white;
+  }
+
+  .slick-list {
+    overflow: initial;
+  }
+
+  .slick-prev {
+    left: -75px;
+  }
+
+  .slick-next {
+    right: -75px;
+  }
+`;
+
+const Wrap = styled.div`
+  position: relative;
+  border-radius: 4px;
+  cursor: pointer;
+
+  a {
+    border-radius: 4px;
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+      rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  }
+`;
 
 export default ImageSlider;

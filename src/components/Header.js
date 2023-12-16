f01fc6dfe612429d8eb77c882edfbc74350f750a
krispikris <kris@kristopherhan.com>
2023-12-15 20:24:17 -0800

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 import { onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth';
-// import { useNavigate } from 'react-router';
-// import { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { auth, provider } from '../firebase';
+import { useNavigate } from 'react-router';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { auth, provider } from '../firebase';
 import {
   setUserLoginDetails,
   setSignOutState,

2013cb38de9b23493c58ad38b38aa3104720e9bb
krispikris <kris@kristopherhan.com>
2024-04-17 23:58:11 -0700

    contain

--- src/components/Recommends.js
+++ src/components/Recommends.js
@@ -7,20 +7,20 @@ const Recommends = (props) => {
   const movies = useSelector(selectRecommend);
 
   return (
-    <Container>
-      <h4>Recommended for You</h4>
-      <Content>
-        {movies &&
-          movies.map((movie, key) => (
-            <Wrap key={key}>
-              {movie.id}
-              <Link to={`/detail/${movie.id}`}>
-                <img src={movie.cardImg} alt="" />
-              </Link>
-            </Wrap>
-          ))}
-      </Content>
-    </Container>
+    // <Container>
+    //   <h4>Recommended for You</h4>
+    //   <Content>
+    //     {movies &&
+    //       movies.map((movie, key) => (
+    //         <Wrap key={key}>
+    //           {movie.id}
+    //           <Link to={`/detail/${movie.id}`}>
+    //             <img src={movie.cardImg} alt="" />
+    //           </Link>
+    //         </Wrap>
+    //       ))}
+    //   </Content>
+    // </Container>
   );
 };
 
